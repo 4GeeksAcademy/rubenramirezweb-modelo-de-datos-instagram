3a12281a0e3e98a23e26a50bc7f51f0d5a15bae7
Rubén Ramírez <rruben084@gmail.com>
2025-04-01 01:04:51 +0000

    Avance

--- src/models.py
+++ src/models.py
@@ -1,9 +1,6 @@
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import String, Boolean, Integer, Foreignkey, Table, Column
+from sqlalchemy import String, Boolean, Integer, ForeignKey, Table, Column
 from sqlalchemy.orm import Mapped, mapped_column, relationship
-from eralchemy2 import render_er
-
-# Una Tabla User -> para registrar usuarios
 
 db = SQLAlchemy()
 
@@ -18,5 +15,68 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "is_active" : self.is_active
             # do not serialize the password, its a security breach
         }
+
+
+# Relacion uno a uno
+class Persona(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(80), nullable=False)
+    pasaporte_id: Mapped[int] = mapped_column(Integer, ForeignKey('pasaporte.id'), unique=True)
+    pasaporte: relationship('Pasaporte', back_populates='persona', uselist=False)
+
+
+class Pasaporte(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    numero: Mapped[str] = mapped_column(String(20))
+
+
+
+
+#### app ####
+
+persona = Persona(nombre="Carlos")
+pasaporte = Pasaporte(numero='ABC123')
+
+persona.pasaporte = pasaporte
+
+db.session.add(persona)
+db.session.commit()
+print(persona.pasaporte.numer) # ABC123
+print(pasaporte.persona.nombre) # Carlos
+
+# Relacion uno a muchos
+
+class Autor(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(80))
+    # Relacion
+    libros: relationship('Libro', back_populates='autor')
+
+
+
+class Libro(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    titulo: Mapped[str] = mapped_column(String(120))
+    autor_id: Mapped[int] = mapped_column(Integer, ForeignKey('autor.id'))
+    
+
+# Relacion muchos a muchos
+
+# Tabla intermedia
+inscripciones = Table('inscripciones',
+        Column("estudiante_id", Integer, ForeignKey('estudiante.id'), primary_key=True),
+        Column('curso_id', Integer, ForeignKey('curso.id'), primary_key=True)
+)
+
+class Estudiante(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(80))
+    cursos: relationship('Curso', sencondary=inscripciones, back_populates='estudiantes')
+
+
+class Curso(db.Model):
+    id: Mapped[int] = mapped_column(primary_key=True)
+    nombre: Mapped[str] = mapped_column(String(80))
